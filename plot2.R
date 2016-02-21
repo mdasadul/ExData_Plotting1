f9b2c6a7cb5a65468bcd3e82c9394516de890b1e
asad <mdasadul@ualberta.ca>
2016-02-21 17:25:40 -0500

    small fil on ploat2

--- plot2.R
+++ plot2.R
@@ -5,5 +5,5 @@ x <- read.table(dataFile, header=TRUE, sep =";",stringsAsFactors=FALSE, dec=".")
 data1<-x[x$Date %in% c("1/2/2007","2/2/2007"),]
 timef<-strptime(paste(data1$Date,data1$Time),"%d/%m/%Y %H:%M:%S")
 png("plot2.png", width=480, height=480)
-plot(timef,as.numeric(data1$Global_active_power),type="l",xlab="",ylab="Global Active Power")
+plot(timef,as.numeric(data1$Global_active_power),type="l",xlab="",ylab="Global Active Power(kilowatts)")
 dev.off()
